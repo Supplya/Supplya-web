2b33deb3a64aed9a1bcb8189b55555939b3062db
PiusAsh <pius1ash@gmail.com>
2024-06-11 00:15:46 +0100

    working on vendor

--- src/app/core/vendor/vendor-settings/vendor-settings.component.ts
+++ src/app/core/vendor/vendor-settings/vendor-settings.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-vendor-settings',
   templateUrl: './vendor-settings.component.html',
-  styleUrls: ['./vendor-settings.component.scss']
+  styleUrls: ['./vendor-settings.component.scss'],
 })
 export class VendorSettingsComponent {
+  selectedTab: string = 'shop';
 
+  selectTab(tab: string) {
+    this.selectedTab = tab;
+  }
 }
