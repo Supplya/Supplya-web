510f9e20b71c9aed1efdb4f291c4d0e2a45be13f
PiusAsh <pius1ash@gmail.com>
2024-06-17 17:00:13 +0100

    worked on homepage

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/authentication/service/auth.service';
+import { CartService } from 'src/app/core/operation/services/cart/cart.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -9,9 +10,24 @@ import Swal from 'sweetalert2';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  constructor(private route: Router, private authService: AuthService) {}
+  cartQuantity: number = 1;
+  previousCartQuantity: number = 0;
+  constructor(
+    private route: Router,
+    private authService: AuthService,
+    private cartService: CartService
+  ) {
+    this.cartService.getCartObservable().subscribe((newCart) => {
+      this.cartQuantity = newCart.totalCount;
+      const newQuantity = newCart.totalCount;
+      console.log(this.cartQuantity, 'newQuantity');
+      console.log(newCart, 'newCart');
+
+      this.previousCartQuantity = newQuantity; // Update previous quantity
+    });
+  }
   isLoggedIn = false;
-  userDetails: any;
+  userDetails: any = null;
   ngOnInit(): void {
     this.userDetails = this.authService.getUserCredentials();
 
