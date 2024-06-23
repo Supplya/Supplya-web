86752c060165357dec21cfa6d71c213ba5166b88
PiusAsh <pius1ash@gmail.com>
2024-06-23 16:20:10 +0100

    updated client id

--- src/assets/environment/environment.prod.ts
+++ src/assets/environment/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
     uploadPreset: 'supplya-uploads',
   },
   google_Client:
-    '143477134998-ufm9i6602uoctn90m1fbauqnusgimphn.apps.googleusercontent.com',
+    'http://948475767702-6ib3iq44k364pie2vibc0o1o59n8io57.apps.googleusercontent.com',
 };
