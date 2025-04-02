baa681f2fd872bf7c542f50a59819e10dc1f3cbf
PiusAsh <pius1ash@gmail.com>
2025-04-02 12:05:24 +0100

    Update

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ProductService } from 'src/app/core/operation/services/product/product.service';
 
 @Component({
   selector: 'app-landing-page',
@@ -11,7 +12,7 @@ export class LandingPageComponent {
   vendorCount: number = 0;
   retailerCount: number = 0;
   manufacturerCount: number = 0;
-  constructor(private route: Router,) {
+  constructor(private route: Router, private productService: ProductService) {
   
 }
   ngOnInit(): void {
@@ -19,6 +20,7 @@ export class LandingPageComponent {
     this.animateCount('vendorCount', 120);
     this.animateCount('retailerCount', 500);
     this.animateCount('manufacturerCount', 8);
+    this.getAllMedia();
   }
   partnerLogos = [
     '/assets/partner-logos/54co.png',
@@ -54,6 +56,27 @@ export class LandingPageComponent {
     this.route.navigate(['/auth']);
     window.scrollTo(0, 0);
   }
+  errorFetchingMedia
+  banners
+  loading
+  getAllMedia() {
+    this.loading = true;
+    this.productService.getAllMedia(1, 50).subscribe(
+      (data: any) => {
+        this.banners = data?.data;
+        this.loading = false;
+      },
+      (error) => {
+        this.errorFetchingMedia = true;
+        this.loading = false;
+      }
+    );
+  }
+  getBanner(tag: string) {
+    return this.banners?.find(banner => banner?.tag?.toLowerCase() === tag?.toLowerCase());
+  }
+
+
   shop() {
     this.route.navigate(['/shop']);
     window.scrollTo(0, 0);
